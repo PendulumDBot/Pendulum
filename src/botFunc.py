c2bc3495edd767c2f34a8777194a3e70f02f4037
York <yorkmerrick@outlook.com>
2023-04-07 11:30:21 +0100

    Counters added for performance

--- src/botFunc.py
+++ src/botFunc.py
@@ -2,13 +2,18 @@ from datetime import datetime
 from geopy.geocoders import Nominatim
 from timezonefinder import TimezoneFinder
 import pytz
+from time import perf_counter
 
 global TF
 TF = TimezoneFinder()
 
 def getTime(location):
+    tstart = perf_counter()
     geolocator = Nominatim(user_agent="pendulum")
     coords = geolocator.geocode(location)
     tz = TF.timezone_at(lng=coords.longitude, lat=coords.latitude)
+    tend = perf_counter()
+    print(tend-tstart)
     return datetime.now(pytz.timezone(tz))
+    
    
