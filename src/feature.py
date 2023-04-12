42aefccbdd54a384ae253fea8867c13fb4114620
MarcusO17 <marcusonggeye@gmail.com>
2023-04-13 01:02:44 +0800

    improved feature with a more modular style

--- src/feature.py
+++ src/feature.py
@@ -1,5 +1,9 @@
 import json
+from pytz import timezone
 import requests
+from datetime import datetime
+
+FORMAT = "%Y-%b-%d %X"
 
 def geocodeForward(location):
     params = {'q': location,'format':'json'}
@@ -7,8 +11,25 @@ def geocodeForward(location):
     return json.loads(response.text)[0]
 
 def getLocationInfo(location):
-    return { "LocationName" : location.get("display_name"),
+    return { "locationName" : location.get("display_name"),
                  "lat" : float(location.get("lat")),
                  "lon" : float(location.get("lon"))
                  }
+
+def findTimezone(location):
+    mode = 0
+    lng = location["lon"]
+    lat = location["lat"]
+    response = requests.get(f"http://timezonefinder.michelfe.it/api/{mode}_{lng}_{lat}")
+    return response.text[:]
+
+def getTimeInfo(location):
+    tzLocation = findTimezone(location)
+    timeNow = datetime.now(timezone(tzLocation))
+    location = {"tzName" : tzLocation,
+                "currentTime" : datetime.now(timezone(findTimezone(location))),
+                "UTCOffset" : timeNow.strftime('%z')}
+
+
+print(findTimezone(getLocationInfo(geocodeForward("penang"))))
 
