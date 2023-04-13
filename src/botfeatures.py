c430ba24e9e444eedee4d4228b65fb834b06516c
MarcusO17 <marcusonggeye@gmail.com>
2023-04-13 21:13:52 +0800

    added timeAdd

--- src/botfeatures.py
+++ src/botfeatures.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from dateutil import parser
 import json
 from .featurehelper import geocodeForward, getLocationInfo, getTimeInfo
 from timezonefinder import TimezoneFinder
@@ -6,9 +7,9 @@ import pytz
 from time import perf_counter
 import requests
 
-global TF
+global TF, FORMAT
 TF = TimezoneFinder()
-format = "%Y-%b-%d %X"
+FORMAT = "%Y-%b-%d %X"
 
 def getTime(location):
     tstart = perf_counter()
@@ -34,14 +35,39 @@ def diffTime(initLoc, targetLoc):
     #get Timezone at Coords
     time1 = getTimeInfo(locInfo1)["UTCOffsetHrs"]
     time2 = getTimeInfo(locInfo2)["UTCOffsetHrs"]
+    print(time1,time2)
 
     if(time1 < time2):
         message = (f'{locInfo1["locationName"]} is behind {locInfo2["locationName"]}'
-                   f'by {time2 - time1} hour(s)')
+                   f' by {time2 - time1} hour(s)')
     else:
        message = (f'{locInfo1["locationName"]} is ahead {locInfo2["locationName"]}'
                    f' by {time1 - time2} hour(s)')
 
     
     return message , getTimeInfo(locInfo1)["currentTime"], getTimeInfo(locInfo2)["currentTime"]
-    
+
+def timeAt(time,timezone,location):
+    #parse initial date/time
+    initTime = datetime.strptime(parser.parse(time).strftime(FORMAT),FORMAT)
+    print(initTime)
+    if timezone in pytz.all_timezones:
+        initTz = timezone
+        initTz = getTimeInfo(timezone)
+    else:
+        initTz = geocodeForward(timezone)
+        initTz = getTimeInfo(getLocationInfo(initTz))
+
+   
+    initHRDiff = initTz["UTCOffsetHrs"]
+    print(initHRDiff)
+    timeUTC = initTime + timedelta(hours=-(initHRDiff) if initHRDiff > 0 else initHRDiff)
+
+    targetLoc = getTimeInfo(getLocationInfo(geocodeForward(location)))
+    targetHRDiff = targetLoc["UTCOffsetHrs"]
+    targetTime = timeUTC + timedelta(hours=targetHRDiff)
+
+    return targetTime
+
+print(diffTime("BKK","Malaysia"))
+print(timeAt("9:09 pm","GMT","UK"))
