14cae89863822abc379d5a36d70e41a55b1c00ad
York <yorkmerrick@outlook.com>
2023-04-19 04:00:09 +0100

    added invalid test for arrowFromWindDirection

--- tests/test_feature_helper.py
+++ tests/test_feature_helper.py
@@ -11,5 +11,8 @@ arrowList = [
 def test_arrowFromWindDirection(input, out) -> None:
     assert arrowFromWindDirection(input) == out
 
+def test_arrowFromWindDirection_invalid() -> None:
+    assert arrowFromWindDirection(370) == 'Invalid wind direction'
+
 def test_geocodeForward() -> None:
     pass
