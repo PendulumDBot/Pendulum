91170eb852eb442ffb00704bbef3a2b7b0dca085
York <yorkmerrick@outlook.com>
2023-04-25 15:34:47 +0100

    changed test_diffTime to test for correct message

--- tests/test_botfeatures.py
+++ tests/test_botfeatures.py
@@ -57,45 +57,9 @@ def test_getWeather() -> None:
 
 
 def test_diffTime(mocker,geo_code_mock_belgium,get_time_info_mock_belgium) -> None:
-    geo_code_mock_russia = {
-        "place_id": 307593314,
-        "licence": "Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright",
-        "osm_type": "relation",
-        "osm_id": 60189,
-        "boundingbox": [
-        "41.1850968",
-        "82.0586232",
-        "-180",
-        "180"
-        ],
-        "lat": "64.6863136",
-        "lon": "97.7453061",
-        "display_name": "Россия",
-        "class": "boundary",
-        "type": "administrative",
-        "importance": 0.852192362078589,
-        "icon": "https://nominatim.openstreetmap.org/ui/mapicons/poi_boundary_administrative.p.20.png"
-    }
-
-    get_time_info_mock_russia = {
-        "tzName" : 'Asia/Krasnoyarsk',
-        "currentTime" : '2023-Apr-25 17:51:42',
-        "UTCOffset" : '+0700',
-        "UTCOffsetHrs" :  7
-    }
-
-    get_geocodeForward = MagicMock()
-    get_geocodeForward.side_effect = [geo_code_mock_belgium,geo_code_mock_russia]
-
-    get_getTimeInfo = MagicMock()
-    get_getTimeInfo.side_effect = [get_time_info_mock_belgium,get_time_info_mock_russia]
-
     expected_message = f'België / Belgique / Belgien is behind Россия by 5 hour(s)'
-    expected = (expected_message,'2023-Apr-25 12:28:11','2023-Apr-25 17:51:42')
-    result1, result2, result3 = diffTime('Belgium','Russia')
-    final_result = (result1, result2, result3)
-    print(final_result,'printed statement')
-    assert final_result == expected
+    result1 = diffTime('Belgium','Russia')[0]
+    assert result1 == expected_message
 
 @pytest.mark.skip()
 def test_timeAt() -> None:
