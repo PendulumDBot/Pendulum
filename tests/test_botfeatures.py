73f2b41e5563ba9111b1e887fe67d04833f7f91f
York <yorkmerrick@outlook.com>
2023-04-25 11:16:34 +0100

    created tests and placeholders for botfeatures

---
+++ tests/test_botfeatures.py
@@ -0,0 +1,15 @@
+import pytest
+from src.botfeatures import getTime, getWeather, diffTime, timeAt
+
+def test_getTime() -> None:
+    expected = ()
+    pass
+
+def test_getWeather() -> None:
+    pass
+
+def test_diffTime() -> None:
+    pass
+
+def test_timeAt() -> None:
+    pass
